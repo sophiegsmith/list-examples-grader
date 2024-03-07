5996a7e67194acaacd5297816b691a889349ed9d
sophiegsmith <sophgsuds@gmail.com>
2024-03-07 08:53:01 -0800

    edited ListExamples.java

--- TestListExamples.java
+++ TestListExamples.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.*;
 import org.junit.*;
 import java.util.Arrays;
 import java.util.List;
+import java.util.StringJoiner;
 
 class IsMoon implements StringChecker {
   public boolean checkString(String s) {
@@ -16,6 +17,24 @@ public class TestListExamples {
     List<String> right = Arrays.asList("a", "d");
     List<String> merged = ListExamples.merge(left, right);
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
+    assertEquals(expected, merged);
+  }
+
+  @Test(timeout = 500)
+  public void testMergeLeftEnd() {
+    List<String> left = Arrays.asList("a", "d");
+    List<String> right = Arrays.asList("a", "b", "c");
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
+    assertEquals(expected, merged);
+  }
+
+  @Test(timeout = 500)
+  public void testMergeEmpty() {
+    List<String> left = Arrays.asList();
+    List<String> right = Arrays.asList();
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList();
     assertEquals(expected, merged);
   }
 }
